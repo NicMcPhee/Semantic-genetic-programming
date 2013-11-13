1f3837fe2ee78cd201291ad432ea3a818894561a
David Donatucci <donat056@morris.umn.edu>
2013-11-13 17:02:49 -0600

    Added Function for determining if a node is a Closure Added Function to find number of parameters for a Closure Added Function to find end parameter of subtree Added Function to find a subtree
    Added tests to see if crossoverparameters works
    Added tests to see if findSubTrees works

--- src/umm/sematicgp/GpTree.groovy
+++ src/umm/sematicgp/GpTree.groovy
@@ -6,11 +6,11 @@ class GpTree {
 	def GpTree(nodes) {
 		this.nodes = nodes
 	}
-	
+
 	def evaluate(context, index = 0) {
 		while (nodes.size > 3 && index < nodes.size - 2) {
 			if (nodes[index] instanceof Closure && !(nodes[index + 1] instanceof Closure) &&
-				!(nodes[index + 2] instanceof Closure)) {
+			!(nodes[index + 2] instanceof Closure)) {
 				def simpleNodes = new GpTree([nodes[index], nodes[index + 1], nodes[index + 2]])
 				nodes[index] = simpleNodes.evaluate(context)
 				nodes.remove(index + 1)
@@ -22,15 +22,48 @@ class GpTree {
 		}
 		if (nodes[index] instanceof Closure) {
 			def f = nodes[index]
-            def x = evaluate(context, index + 1)
-            def y = evaluate(context, index + 2)
-            return f(x, y)
-        } else {
-            if(context.containsKey(nodes[index])) {
+			def x = evaluate(context, index + 1)
+			def y = evaluate(context, index + 2)
+			return f(x, y)
+		} else {
+			if(context.containsKey(nodes[index])) {
 				return context.(nodes[index])
-            } else {
-                return nodes[index]
-            }
-        }
-    }
+			} else {
+				return nodes[index]
+			}
+		}
+	}
+
+	def findSubTree(int i) {
+		return nodes[i..findCrossoverParameters(i)]
+	}
+
+	def isFunction(node) {
+		(node instanceof Closure)
+	}
+
+	def numArgs(node) {
+		if (isFunction(node)) {
+			2
+		}
+		else {
+			0
+		}
+	}
+	def findCrossoverParameters(int i) {
+		def j = 0
+		def end = i
+		while(j < numArgs(nodes[i])) {
+			end = findCrossoverParameters(end + 1)
+			j++;
+		}
+		return end 
+	}
 }
+
+//if (v instanceof Closure){
+//	switch (v.parameterTypes.length) {
+//	 case 0: newValue = v(); break;
+//	 case 1: newValue = v(entity[k]); break; // if one params, the closure is called with the field value
+//	 case 2: newValue = v(entity[k],entity); break; // if two params, the closure is called with teh field value and the entity
+//	}
--- test/umm/semanticgp/GpTreeEvaluationTest.groovy
+++ test/umm/semanticgp/GpTreeEvaluationTest.groovy
@@ -9,11 +9,19 @@ import umm.sematicgp.GpTree
 import umm.sematicgp.Operator;
 
 class GpTreeEvaluationTest extends Specification {
-	def plus = { x, y -> x + y }
-	def sub =  { x, y -> x - y }
-	def mult =  { x, y -> x * y }
-	def divi =  { x, y -> x / y }
-	
+	def plus = {
+		x, y -> x + y
+	}
+	def sub =  {
+		x, y -> x - y
+	}
+	def mult =  {
+		x, y -> x * y
+	}
+	def divi =  {
+		x, y -> x / y
+	}
+
 
 	@Test
 	public void constantTest() {
@@ -21,33 +29,33 @@ class GpTreeEvaluationTest extends Specification {
 		def context = [:]
 		def tree5 = new GpTree([5])
 		def tree7 = new GpTree([7])
-		
+
 		when:
 		def result5 = tree5.evaluate(context)
 		def result7 = tree7.evaluate(context)
-		
+
 		then:
 		result5 == 5
 		result7 == 7
-		
 	}
-	
+
 	@Test
 	public void variableTest() {
 		given:
 		def context = ['x': 4, 'y' : 6]
+
 		def treex = new GpTree(["x"])
 		def treey = new GpTree(["y"])
-		
-		when: 
+
+		when:
 		def resultx = treex.evaluate(context)
 		def resulty = treey.evaluate(context)
-		
+
 		then:
 		resultx == 4
 		resulty == 6
 	}
-	
+
 	@Test
 	public void simpleConstantTreeTest() {
 		given:
@@ -56,20 +64,19 @@ class GpTreeEvaluationTest extends Specification {
 		def constantSubTree = new GpTree([sub, 4, 1])
 		def constantMultTree = new GpTree([mult, 2, 3])
 		def constantDiviTree = new GpTree([divi, 6, 3])
-		
 		when:
 		def resultAddTree = constantAddTree.evaluate(context)
 		def resultSubTree = constantSubTree.evaluate(context)
 		def resultMultTree = constantMultTree.evaluate(context)
 		def resultDiviTree = constantDiviTree.evaluate(context)
-		
-		then: 
+
+		then:
 		resultAddTree == 1
 		resultSubTree == 3
 		resultMultTree == 6
 		resultDiviTree == 2
 	}
-	
+
 	@Test
 	public void simpleVariableTreeTest () {
 		given:
@@ -86,7 +93,7 @@ class GpTreeEvaluationTest extends Specification {
 		def firstVarDiviTree = new GpTree ([divi, "x", 1])
 		def lastVarDiviTree = new GpTree ([divi, 3, "x"])
 		def twoVarDiviTree = new GpTree ([divi, "x" , "y"])
-		
+
 		when:
 		def resultFirstAddTree = firstVarAddTree.evaluate(context)
 		def resultLastAddTree = lastVarAddTree.evaluate(context)
@@ -100,7 +107,7 @@ class GpTreeEvaluationTest extends Specification {
 		def resultFirstDiviTree = firstVarDiviTree.evaluate(context)
 		def resultLastDiviTree = lastVarDiviTree.evaluate(context)
 		def resultTwoVarDiviTree = twoVarDiviTree.evaluate(context)
-		
+
 		then:
 		resultFirstAddTree == 4
 		resultLastAddTree == 4
@@ -115,69 +122,107 @@ class GpTreeEvaluationTest extends Specification {
 		resultLastDiviTree == 1
 		resultTwoVarDiviTree == 1/2
 	}
-	
+
 	@Test
 	public void threeOperatorConstantTreeTest() {
 		given:
 		def context = [:]
 		def threeOperatorTree = new GpTree([mult, plus, 2, 3, sub, 6, 5])
 		def threeAdditionTree = new GpTree([plus, plus, 1, 2, plus, 3, 4])
-		
+
 		when:
 		def resultThreeOperatorTree = threeOperatorTree.evaluate(context)
 		def resultThreeAdditionTree = threeAdditionTree.evaluate(context)
-		
+
 		then:
 		resultThreeOperatorTree == 5
 		resultThreeAdditionTree == 10
 	}
-	
+
 	@Test
 	public void threeOperatorVariableTreeTest() {
 		given:
 		def context = ['x':3, 'y':5]
 		def threeOperatorTree = new GpTree([mult, plus, 2, 'x', sub, 6, 'y'])
 		def threeAdditionTree = new GpTree([plus, plus, 1, 2, plus, 'x', 4])
-		
+
 		when:
 		def resultThreeOperatorTree = threeOperatorTree.evaluate(context)
 		def resultThreeAdditionTree = threeAdditionTree.evaluate(context)
-		
+
 		then:
 		resultThreeOperatorTree == 5
 		resultThreeAdditionTree == 10
 	}
-	
+
 	@Test
 	public void complexOperatorConstantTreeTest() {
 		given:
 		def context = [:]
 		def threeOperatorTree = new GpTree([mult, plus, plus, 1, 1, 3, sub, 6, divi, 10, 2])
 		def threeAdditionTree = new GpTree([plus, plus, 1, plus, 1, 1, plus, plus, 1, plus, 1, 1, 4])
-		
+
 		when:
 		def resultThreeOperatorTree = threeOperatorTree.evaluate(context)
 		def resultThreeAdditionTree = threeAdditionTree.evaluate(context)
-		
+
 		then:
 		resultThreeOperatorTree == 5
 		resultThreeAdditionTree == 10
 	}
-	
+
 	@Test
 	public void complexOperatorVariableTreeTest() {
 		given:
 		def context = ['x': 2, 'y': 6, 'z': 1]
 		def threeOperatorTree = new GpTree([mult, plus, plus, 'z', 'z', 3, sub, 'y', divi, 10, 'x'])
-		def threeAdditionTree = 
-			new GpTree([plus, plus, 'z', plus, 'z', 'z', plus, plus, 'z', plus, 'z', 'z', 4])
-		
+		def threeAdditionTree =
+		new GpTree([plus, plus, 'z', plus, 'z', 'z', plus, plus, 'z', plus, 'z', 'z', 4])
+
 		when:
 		def resultThreeOperatorTree = threeOperatorTree.evaluate(context)
 		def resultThreeAdditionTree = threeAdditionTree.evaluate(context)
-		
+
 		then:
 		resultThreeOperatorTree == 5
 		resultThreeAdditionTree == 10
+	}
+
+	@Test 
+	public void findCrossOverParameters() {
+		given: 
+		def threeOperatorTree = new GpTree([mult, plus, plus, 'z', 'z', 3, sub, 'y', divi, 10, 'x'])
+		
+		when:
+		def resultSingleCParameter = threeOperatorTree.findCrossoverParameters(5)
+		def resultSingleVParameter = threeOperatorTree.findCrossoverParameters(4)
+		def resultSingleOParameter = threeOperatorTree.findCrossoverParameters(8)
+		def resultMultipleOParameter = threeOperatorTree.findCrossoverParameters(1)
+		
+		then:
+		resultSingleCParameter == 5
+		resultSingleVParameter == 4
+		resultSingleOParameter == 10
+		resultMultipleOParameter == 5
+	}
+	
+	@Test
+	public void findSubTrees() {
+		given:
+
+	def context = ['x': 2]
+	def threeOperatorTree = new GpTree([mult, plus, plus, 'z', 'z', 3, sub, 'y', divi, 10, 'x'])
+
+	when:
+	def resultConstantSubTree = threeOperatorTree.findSubTree(5)
+	def resultVariableSubTree = threeOperatorTree.findSubTree(4)
+	def resultOneOpSubTree = threeOperatorTree.findSubTree(8)
+	def resultMultOpSubTree = threeOperatorTree.findSubTree(1)
+	
+	then:
+	resultConstantSubTree == threeOperatorTree.nodes[5..5]
+	resultVariableSubTree == threeOperatorTree.nodes[4..4]
+	resultOneOpSubTree == threeOperatorTree.nodes[8..10]
+	resultMultOpSubTree == threeOperatorTree.nodes[1..5]
 	}
 }
