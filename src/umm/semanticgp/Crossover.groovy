cc8cecfe8fd922f91dbdd9693d5d2ee22de485a4
Nic McPhee <mcphee@morris.umn.edu>
2013-12-06 10:57:33 -0600

    Fixed crossover class so it runs and passes tests. It still needs mutation and reproduction.
    This is all work that David did, but is being committed by me because of
    a git problem on his computer.

--- src/umm/semanticgp/Crossover.groovy
+++ src/umm/semanticgp/Crossover.groovy
@@ -5,66 +5,73 @@ import java.util.Random;
 // 90% or more crossover 1% mutation rest copy into next generation.
 class Crossover {
 
-	Random rand = new Random()
-	def percentage = rand.nextInt(100)
+    static Random rand = new Random()
+    def percentage = rand.nextInt(100)
 
-	def crossover(GpTree P1, GpTree P2) {
-		def l2opP1 = rand.nextInt(10)
-		def l2opP2 = rand.nextInt(10)
-		def parent1Leaves = []
-		def parent2Leaves = []
-		def parent1Operators = []
-		def parent2Operators = []
-		// adds indices of P1
-		for (int i = 0; i < P1.nodes.size(); i++) {
-			if (P1.nodes[i] instanceof Closure) {
-				parent1Operators.add(i)
-			}
-			else {
-				parent1Leaves.add(i)
-			}
-		}
-		// adds indices of P2
-		for (int i = 0; i < P2.nodes.size(); i++) {
-			if (P2.nodes[i] instanceof Closure) {
-				parent2Operators.add(i)
-			}
-			else {
-				parent2Leaves.add(i)
-			}
-		}
+    static crossover(GpTree P1, GpTree P2) {
+        def copyP1 = new GpTree(P1.nodes.clone())
+        
+        
+        def l2opP1 = rand.nextInt(10)
+        def l2opP2 = rand.nextInt(10)
+        def parent1Leaves = []
+        def parent2Leaves = []
+        def parent1Operators = []
+        def parent2Operators = []
+        // adds indices of P1
+        for (int i = 0; i < P1.nodes.size(); i++) {
+            if (copyP1.nodes[i] instanceof Closure) {
+                parent1Operators.add(i)
+            }
+            else {
+                parent1Leaves.add(i)
+            }
+        }
+        // adds indices of P2
+        for (int i = 0; i < P2.nodes.size(); i++) {
+            if (P2.nodes[i] instanceof Closure) {
+                parent2Operators.add(i)
+            }
+            else {
+                parent2Leaves.add(i)
+            }
+        }
 
-		if (l2opP1 < 1) {
-			def leafIndexToAdd = parent1Leaves[rand.nextInt(parent1Leaves.size())]
-			P1.nodes.remove(leafIndexToAdd)
-			if (l2opP2 < 1) {
-				P1.nodes.addAll(leafIndexToAdd + 1, P2.nodes[parent2Leaves[rand.nextInt(parent2Leaves.size())]])
-			}
-			else {
-				P1.nodes.addAll(leafIndexToAdd + 1,P2.findSubtree(parent2Operators[rand.nextInt(parent2Operators.size())]))
-			}
-		}
-		else {
-			def opIndexToAdd = parent1Operators[rand.nextInt(parent1Operators.size())]
-			P1.nodes.remove(findSubtree(opIndexToAdd))
-			if (l2opP2 < 1) {
-				P1.nodes.addAll(opIndexToAdd + 1, P2.nodes[parent2Leaves[rand.nextInt(parent2Leaves.size())]])
-			}
-			else {
-				P1.nodes.addAll(opIndexToAdd + 1,P2.findSubtree(parent2Operators[rand.nextInt(parent2Operators.size())]))
-			}
-		}
-	}
+        if (l2opP1 < 1) {
+            def leafIndexToAdd = parent1Leaves[rand.nextInt(parent1Leaves.size())]
+            copyP1.nodes.remove(leafIndexToAdd)
+            
+            if (l2opP2 < 1) {
+                copyP1.nodes.addAll(leafIndexToAdd, P2.nodes[parent2Leaves[rand.nextInt(parent2Leaves.size())]])
+            }
+            else {
+                copyP1.nodes.addAll(leafIndexToAdd,P2.findSubTree(parent2Operators[rand.nextInt(parent2Operators.size())]))
+            }
+        }
+        else {
+            def opIndexToAdd = parent1Operators[rand.nextInt(parent1Operators.size())]
+            def endIndex = copyP1.findCrossoverParameters(opIndexToAdd) + 1
+            copyP1.nodes.removeRange(opIndexToAdd,endIndex)
+            if (l2opP2 < 1) {
+                copyP1.nodes.addAll(opIndexToAdd, P2.nodes[parent2Leaves[rand.nextInt(parent2Leaves.size())]])
+            }
+            else {
+                copyP1.nodes.addAll(opIndexToAdd,P2.findSubTree(parent2Operators[rand.nextInt(parent2Operators.size())]))
+            }
+        }
+        return copyP1
+    }
+    
 }
 
-//	static main(args) {
-//		if (percentage < 1) {
-//			mutation(P1)
-//		}
-//		else if (percentage < 91) { //90% crossover rate.
-//			crossover(P1,P2)
-//		}
-//		else {
-//			reproduction(P1)
-//		}
+//  static main(args) {
+//      if (percentage < 1) {
+//          mutation(P1)
+//      }
+//      else if (percentage < 91) { //90% crossover rate.
+//          crossover(P1,P2)
+//      }
+//      else {
+//          reproduction(P1)
+//      }
 //}
---
+++ test/umm/semanticgp/CrossoverTest.groovy
@@ -0,0 +1,42 @@
+package umm.semanticgp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import umm.semanticgp.GpTree
+import spock.lang.Specification;
+
+class CrossoverTest extends Specification {
+
+    @Test
+    public void simpleCrossOver() {
+        given:
+        def P1 = new GpTree([Operator.mult, 0, Operator.divi, 1, 2])
+        def P2 = new GpTree([Operator.sub, 3, Operator.plus, 4, 5])
+        
+        when:
+        def childTree = Crossover.crossover(P1,P2)
+        println(P1.printGpTree())
+        println(P2.printGpTree())
+        println(childTree.printGpTree())
+        then:
+        assert (1..9).contains(childTree.nodes.size())
+
+    }
+    
+    @Test
+    public void longerCrossOver() {
+        given:
+        def P1 = new GpTree([Operator.mult, 0, Operator.divi, 1, Operator.log, Operator.gpif, 1, 3, 4])
+        def P2 = new GpTree([Operator.sub, "y", Operator.plus, "z", Operator.gpif, "x", "w", Operator.cos, "v"])
+        
+        when:
+        def childTree = Crossover.crossover(P1,P2)
+        println(P1.printGpTree())
+        println(P2.printGpTree())
+        println(childTree.printGpTree())
+        then:
+        assert (1..17).contains(childTree.nodes.size())
+
+    }
+}
